ec9b02c9116dac7b49489c760c8b57d01507153d
Michael Troger <michaeltroger@users.noreply.github.com>
2017-05-30 19:04:55 +0200

    recognizing stairs

--- AndroidProject/app/src/main/java/com/michaeltroger/simpleactivityrecognition/SensorService.java
+++ AndroidProject/app/src/main/java/com/michaeltroger/simpleactivityrecognition/SensorService.java
@@ -1,9 +1,6 @@
 package com.michaeltroger.simpleactivityrecognition;
 
-import android.app.Notification;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -11,31 +8,42 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.IBinder;
-import android.os.PowerManager;
 import android.util.Log;
 
 public class SensorService extends Service implements SensorEventListener{
-    private static final int ONGOING_NOTIFICATION_ID = 1;
     private SensorManager mSensorManager;
-    private Sensor mSensor;
-    private MediaPlayer mMediaplayer;
-    private float mEwma = 0;
-    private static final float ALPHA = 0.5f;
-    private static final float THRESHOLD = 0.1f;
+    private MediaPlayer mMediaplayerMoving;
+    private float mEwmaAccelleration = 0;
+    private static final float ALPHA_ACCELERATION = 0.5f;
+    private static final float THRESHOLD_EWMA_ACCELERATION = 0.1f;
 
     private static final String TAG = SensorService.class.getSimpleName();
     private boolean mMoving;
+    private float mEwmaPressure = 0;
+    private static final float ALPHA_PRESSURE = 0.5f;
+    private static float THRESHOLD_EWMA_PRESSURE = 0.2f;
+    private float mInitialPressure;
+    private MediaPlayer mMediaplayerStairs;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 
-        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        Sensor sensorAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        Sensor sensorBarometer = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        mSensorManager.registerListener(this, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, sensorBarometer, SensorManager.SENSOR_DELAY_NORMAL);
 
-        mMediaplayer = MediaPlayer.create(getApplicationContext(), R.raw.robot);
+        mMediaplayerMoving = MediaPlayer.create(getApplicationContext(), R.raw.robot);
+        mMediaplayerStairs = MediaPlayer.create(getApplicationContext(), R.raw.typing);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mSensorManager.unregisterListener(this);
     }
 
     @Override
@@ -51,18 +59,29 @@ public class SensorService extends Service implements SensorEventListener{
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            handleAccelerationChanged(event);
+        } else if (event.sensor.getType() == Sensor.TYPE_PRESSURE) {
+            handlePressureChanged(event);
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+    void handleAccelerationChanged(SensorEvent event) {
         float accX = event.values[0];
         float accY = event.values[1];
         float accZ = event.values[2];
 
         int accVectorLength = (int)Math.round(Math.sqrt(accX * accX + accY * accY + accZ * accZ));
 
-        mEwma = ALPHA * accVectorLength + (1 - ALPHA) * mEwma;
+        mEwmaAccelleration = ALPHA_ACCELERATION * accVectorLength + (1 - ALPHA_ACCELERATION) * mEwmaAccelleration;
 
-        Log.d(TAG, "ewma: " + mEwma);
-        if (mEwma > THRESHOLD) {
-            if (!mMoving && !mMediaplayer.isPlaying()) {
-                mMediaplayer.start();
+        if (mEwmaAccelleration > THRESHOLD_EWMA_ACCELERATION) {
+            if (!mMoving) {
+                mMediaplayerMoving.start();
                 mMoving = true;
             }
         } else {
@@ -70,6 +89,22 @@ public class SensorService extends Service implements SensorEventListener{
         }
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+    private void handlePressureChanged(SensorEvent event) {
+        float pressure = event.values[0];
+
+        if (mEwmaPressure == 0) {
+            mInitialPressure = pressure;
+            mEwmaPressure = pressure;
+        } else {
+            mEwmaPressure = ALPHA_PRESSURE * pressure + (1 - ALPHA_PRESSURE) * mEwmaPressure;
+        }
+        if (mMoving) {
+            if (Math.abs(mInitialPressure - mEwmaPressure) > THRESHOLD_EWMA_PRESSURE) {
+                mMediaplayerStairs.start();
+                mInitialPressure = mEwmaPressure;
+            }
+        }
+
+    }
+
 }
